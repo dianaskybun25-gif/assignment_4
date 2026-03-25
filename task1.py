79fbfea4338633ab6ec62c1105976fa18d1a6b9f
Diana <diana.skybun.25@kse.org.ua>
2026-03-25 08:32:08 +0200

    Введена функція для обрахунку полінома та поліном

---
+++ task1.py
@@ -0,0 +1,9 @@
+def evaluate_polynomial(poly_dict, x)
+    result = 0
+    for i in poly_dict:
+        result = result + poly_dict[i] * x**i
+    return result
+
+
+
+my_poly = {0: -10, 2: 3, 4: 1}
