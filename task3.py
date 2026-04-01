f8db1564ca78930f84dbb15aa2b6f654109c191e
Diana <diana.skybun.25@kse.org.ua>
2026-04-01 11:30:48 +0300

    - видалено зайве з коду

--- task3.py
+++ task3.py
@@ -13,45 +13,6 @@ experiments_data = [
 "time_ms": 2.1}
 ]
 
-def analyze_methods(data):
-    statistics = {}
-    #--------------------
-    n = []
-    e = []
-    r = []
-    o = [n,e,r]
-    for method in data:
-        if method["method"] == "Euler":
-            e.append(method)
-        if method["method"] == "Runge-Kutta":
-            r.append(method)
-        if method["method"] == "Newton":
-            n.append(method)
-    #------------------------------
-    for i in o:
-        stat = {}
-        iterations = len(i)
-        stat["iterations_count"] = iterations
-        tot_time = 0
-        for j in i:
-            tot_time += j["time_ms"]
-        stat["total_time_ms"] = tot_time
-        er = [j["error"] for j in i]
-        m_error = max(er)
-        stat["max_error"] = m_error
-        #----------------------------
-        if i == n:
-            statistics["Newton"] = stat
-        if i == e:
-            statistics["Euler"] = stat
-        if i == r:
-            statistics["Runge-Kutta"] = stat
-        #----------------------------
-    return statistics
-
-statistics_1 = analyze_methods(experiments_data)
-#for method in statistics_1:
-    #print(method, statistics_1[method])
 
 def analyze_methods_update(data):
     statistics_2 = {}
